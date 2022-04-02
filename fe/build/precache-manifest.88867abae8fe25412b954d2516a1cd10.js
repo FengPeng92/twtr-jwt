381b6b1a77f27cc807871273c66c16755cbc64bc
FengPeng92 <peng.fen@northeastern.edu>
2022-04-02 15:40:57 -0400

    correct structure

---
+++ fe/build/precache-manifest.88867abae8fe25412b954d2516a1cd10.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5872d73753ef34e0de1e8f956d5eab39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d5f64552099ab5dc39b",
+    "url": "/static/css/main.7f904051.chunk.css"
+  },
+  {
+    "revision": "ebfac57d4a7aeb84bbd8",
+    "url": "/static/js/2.d2be65ae.chunk.js"
+  },
+  {
+    "revision": "2f90c8634b386e36b9c7e8327c2fcf69",
+    "url": "/static/js/2.d2be65ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5d5f64552099ab5dc39b",
+    "url": "/static/js/main.943beb47.chunk.js"
+  },
+  {
+    "revision": "07ef167960929c3ab583",
+    "url": "/static/js/runtime-main.60388dba.js"
+  }
+]);
